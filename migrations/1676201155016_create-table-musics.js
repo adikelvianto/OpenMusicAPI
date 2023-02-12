7b02ccd5e48c63a1be8ddedc8acdc27fe54fe997
Adi Kelvianto <adikelvianto2906@gmail.com>
2023-02-12 19:30:19 +0700

    add database and current number of fail still 7

---
+++ migrations/1676201155016_create-table-musics.js
@@ -0,0 +1,48 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('musics', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true,
+    },
+    genre: {
+      type: 'TEXT',
+      notNull: false,
+    },
+    performer: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    duration: {
+      type: 'INTEGER',
+      notNull: false,
+    },
+    albumId: {
+      type: 'VARCHAR(50)',
+      notNull: false,
+    },
+    inserted_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('musics');
+};
--- src/services/inMemory/SongsService.js
+++ src/services/inMemory/SongsService.js
@@ -1,11 +1,87 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-console */
 const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
+const { mapDBToModel } = require('../../utils');
 
 class SongsService {
   constructor() {
-    this._song = [];
+    this._pool = new Pool();
+  }
+
+  // Add song service //
+  async addSong({
+    title, year, genre, performer, duration, albumId,
+  }) {
+    const id = nanoid(16);
+    const insertedAt = new Date().toISOString();
+    const updatedAt = insertedAt;
+
+    const query = {
+      text: 'INSERT INTO musics VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id',
+      values: [id, title, year, genre, performer, duration, albumId, insertedAt, updatedAt],
+    };
+    console.log(query.values);
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Lagu gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+
+  // Get song service //
+  async getSongs() {
+    const result = await this._pool.query('SELECT id, title, performer FROM musics');
+    return result.rows;
+  }
+
+  // Get song by Id service //
+  async getSongById(id) {
+    const query = {
+      text: 'SELECT * FROM musics WHERE id = $1',
+      values: [id],
+    };
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('Lagu tidak ditemukan');
+    }
+
+    return result.rows.map(mapDBToModel)[0];
+  }
+
+  // Edit song service //
+  async editSongById(id, {
+    title, year, genre, performer, duration, albumId,
+  }) {
+    const updatedAt = new Date().toISOString();
+    const query = {
+      text: 'UPDATE musics SET title = $1, year = $2, genre = $3, performer = $4, duration = $5, albumId = $6, updated_at = $7 WHERE id = $8 RETURNING id',
+      values: [title, year, genre, performer, duration, albumId, updatedAt, id],
+    };
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Gagal memperbarui lagu. Id tidak ditemukan');
+    }
+  }
+
+  // Delete song service //
+  async deleteSongById(id) {
+    const query = {
+      text: 'DELETE FROM musics WHERE id = $1 RETURNING id',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Lagu gagal dihapus. Id tidak ditemukan');
+    }
   }
 }
 
