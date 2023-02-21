1385c7c29a4ebecc52bbfebf9a59ba547a2fca58
Adi Kelvianto <adikelvianto2906@gmail.com>
2023-02-21 20:32:28 +0700

    create handler for users api

--- src/validator/albums/schema.js
+++ src/validator/albums/schema.js
@@ -2,7 +2,8 @@ const Joi = require('joi');
 
 const AlbumPayloadSchema = Joi.object({
   name: Joi.string().required(),
-  year: Joi.number().integer().required(),
+  // eslint-disable-next-line newline-per-chained-call
+  year: Joi.number().integer().min(1970).max(new Date().getFullYear()).required(),
 });
 
 module.exports = { AlbumPayloadSchema };
