a81de61c6f41734759155208246615c225147b78
Adi Kelvianto <adikelvianto2906@gmail.com>
2023-02-21 20:40:54 +0700

    plugin users done, all test passed

--- src/server.js
+++ src/server.js
@@ -3,16 +3,27 @@
 
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+
+// songs
 const songs = require('./api/songs');
-const albums = require('./api/albums');
 const SongsService = require('./services/postgres/SongsService');
-const AlbumsService = require('./services/postgres/AlbumsService');
 const SongsValidator = require('./validator/songs');
+
+// albums
+const albums = require('./api/albums');
+const AlbumsService = require('./services/postgres/AlbumsService');
 const AlbumsValidator = require('./validator/albums');
+
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
 
 const init = async () => {
   const songsService = new SongsService();
   const albumsService = new AlbumsService();
+  const usersService = new UsersService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -36,6 +47,13 @@ const init = async () => {
       options: {
         service: albumsService,
         validator: AlbumsValidator,
+      },
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
       },
     },
   ]);
